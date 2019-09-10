6e6e0a5898cb17cbcd38cc226644c24bc52a8b69
robbynurman <robbynurman@gmail.com>
2019-09-10 11:51:25 +0700

    initial commit

---
+++ app/src/main/java/com/example/myapplication/FloatingActionButton.java
@@ -0,0 +1,9 @@
+package com.example.myapplication;
+
+import android.view.View;
+
+class FloatingActionButton {
+
+    public void setOnClickListener(View.OnClickListener onClickListener) {
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/List.java
@@ -0,0 +1,9 @@
+package com.example.myapplication;
+
+class List<T> {
+    public void addAll(java.util.List<T> allNotes) {
+    }
+
+    public void add(int i, T n) {
+    }
+}
